12b67974c00fd006400320ecd0a78ffdf41f6870
LIONPARCELBANDUNG-POS101 <lp.bds.citarik.cicalengka@gmail.com>
2026-09-21 08:53:12 +0700

    Create data.js

---
+++ artikel/data.js
@@ -0,0 +1,9 @@
+const artikelData = [
+  {
+    judul: "Cara Kirim Paket yang Aman dan Mudah",
+    deskripsi: "Panduan sederhana mengirim paket agar lebih aman sampai tujuan.",
+    tanggal: "21 September 2026",
+    gambar: "images/cara-kirim-paket.jpg",
+    link: "carakirimpaket.html"
+  }
+];
