995e59c2a6ffb697bdeabec46e8fb7b0b290ceab
LIONPARCELBANDUNG-POS101 <lp.bds.citarik.cicalengka@gmail.com>
2026-09-21 09:05:55 +0700

    Update data.js

--- artikel/data.js
+++ artikel/data.js
@@ -6,4 +6,11 @@ const artikelData = [
     gambar: "images/cara-kirim-paket.jpg",
     link: "carakirimpaket.html"
   }
+  {
+  judul: "Tips Packing Paket agar Aman Sampai Tujuan",
+  deskripsi: "Tips packing paket agar lebih aman sampai tujuan, mulai dari memilih kemasan hingga menutup paket dengan benar.",
+  tanggal: "21 September 2026",
+  gambar: "",
+  link: "tips-packing.html"
+}
 ];
