5c84265be981843cca83277b1020f4c637954621
LIONPARCELBANDUNG-POS101 <lp.bds.citarik.cicalengka@gmail.com>
2026-09-22 15:52:41 +0700

    Update data.js

--- artikel/data.js
+++ artikel/data.js
@@ -1,5 +1,11 @@
 const artikelData = [
  {
+    judul: "TIPS AMAN KIRIM LAPTOP",
+    deskripsi: "YANG PERLU DI SIAPKAN SAAT  KIRIM LAPTOP.",
+    tanggal: "22 September 2026",
+    gambar: "images/cara-kirim-paket.jpg",
+    link: "tipsamankirimlaptop.html"
+  },{
     judul: "TIPS AMAN KIRIM HP",
     deskripsi: "YANG PERLU DI SIAPKAN SAAT  KIRIM HP.",
     tanggal: "22 September 2026",
